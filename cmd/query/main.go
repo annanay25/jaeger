2d276fac47de58d8adef01029b55ff65a62b5b59
Annanay <annanay.a@media.net>
2019-02-27 00:24:42 +0530

    Use cmux, use stream in GetTraceResponse, regenerate *pb.go and swagger doc
    Signed-off-by: Annanay <annanay.a@media.net>

--- cmd/query/main.go
+++ cmd/query/main.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/opentracing/opentracing-go"
+	"github.com/soheilhy/cmux"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	jaegerClientConfig "github.com/uber/jaeger-client-go/config"
@@ -144,42 +145,52 @@ func main() {
 				r.Handle(mBldr.HTTPRoute, h)
 			}
 
-			portStr := ":" + strconv.Itoa(queryOpts.Port)
 			compressHandler := handlers.CompressHandler(r)
 			recoveryHandler := recoveryhandler.NewRecoveryHandler(logger, true)
+
+			// Create HTTP Server
+			httpServer := &http.Server{
+				Handler: recoveryHandler(compressHandler),
+			}
 
 			// Create GRPC Server.
 			grpcServer := grpc.NewServer()
 
-			// Add logging.
 			grpclog.SetLoggerV2(grpclog.NewLoggerV2(ioutil.Discard, os.Stderr, os.Stderr))
 
-			// Register GRPC Handler.
-			spanReaderGRPC, err := storageFactory.CreateSpanReader()
-			if err != nil {
-				logger.Fatal("Failed to create span reader", zap.Error(err))
-			}
-			spanReaderGRPC = storageMetrics.NewReadMetricsDecorator(spanReaderGRPC, baseFactory.Namespace(metrics.NSOptions{Name: "query-grpc", Tags: nil}))
-			dependencyReaderGRPC, err := storageFactory.CreateDependencyReader()
-			if err != nil {
-				logger.Fatal("Failed to create dependency reader", zap.Error(err))
-			}
-
-			grpcHandler := NewGRPCHandler(spanReaderGRPC, dependencyReaderGRPC, apiHandlerOptions)
+			grpcHandler := app.NewGRPCHandler(queryService, logger, tracer)
 			api_v2.RegisterQueryServiceHandler(grpcServer, grpcHandler)
 
-			go func() {
-				logger.Info("Starting HTTP+GRPC server", zap.Int("port", queryOpts.Port))
-				conn, err := net.Listen("tcp", fmt.Sprintf(":%d", queryOpts.Port))
-				if err != nil {
-					logger.Fatal("Could not launch service", zap.Error(err))
-				}
+			// Prepare cmux conn.
+			conn, err := net.Listen("tcp", fmt.Sprintf(":%d", queryOpts.Port))
+			if err != nil {
+				logger.Fatal("Could not start listener", zap.Error(err))
+			}
 
-				// Multiplexed server.
-				// Use CombinedHandler here, which will "reverse-proxy" between HTTP and GRPC backends.
-				srv.Serve(conn, NewCombinedHandler(grpcServer, recoveryHandler(compressHandler)))
+			// Create cmux server.
+			// cmux will reverse-proxy between HTTP and GRPC backends.
+			s := cmux.New(conn)
+
+			// Add GRPC and HTTP listeners.
+			grpcL := s.Match(
+				cmux.HTTP2HeaderField("content-type", "application/grpc"),
+				cmux.HTTP2HeaderField("content-type", "application/grpc+proto"))
+			httpL := s.Match(cmux.Any())
+
+			go func() {
+				logger.Info("Starting HTTP server", zap.Int("port", queryOpts.Port))
+				httpServer.Serve(httpL)
 				hc.Set(healthcheck.Unavailable)
 			}()
+
+			go func() {
+				logger.Info("Starting GRPC server", zap.Int("port", queryOpts.Port))
+				grpcServer.Serve(grpcL)
+				hc.Set(healthcheck.Unavailable)
+			}()
+
+			// Start cmux server.
+			s.Serve()
 
 			hc.Ready()
 			<-serverChannel
